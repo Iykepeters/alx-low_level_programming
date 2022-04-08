8249a9eed384993cf396f536ea9d027489a41e4e
Iykepeters <peterstickz@gmail.com>
2022-04-08 11:50:15 -0700

    this program prints a text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - signifies entry point of our code
+ *
+ * Return: must always be 0 (success)
+ * */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
