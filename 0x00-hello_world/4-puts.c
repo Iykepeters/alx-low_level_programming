e432fdc7a1e61d62a88648938b602e2f50af10b8
Iykepeters <peterstickz@gmail.com>
2022-04-09 08:38:43 -0700

    this program prints a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,9 @@
  * Return: must always be 0 (success)
  *
  */
-
-int main (void) {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+int main (void)
+{ 
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
