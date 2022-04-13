eb5a84ac1240366ed84a289c743933889b339f23
Iykepeters <peterstickz@gmail.com>
2022-04-13 15:07:36 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: ALways 0 
+ */
+
+void print_alphabet(void);
+{
+char letter;
+
+for (letter ='a'; letter <= 'z'; letter++)
+{_putchar(letter);
+}
+
+_putchar('\n');
+}
