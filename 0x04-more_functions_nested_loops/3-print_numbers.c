b752cc4e476a0be0620f88cee37602e3ec58b098
Iykepeters <peterstickz@gmail.com>
2022-04-15 16:42:57 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,15 +4,15 @@
  * Return: Always 0.
  */
 void print_numbers(void)
-{	
+{
+
 int i;
 
 i = 0;
-while (i < 10)
-{
-_putchar (i + '0');
-i++;
-}
+
+for (i = 0; i < 10; i++)
+
+_putchar(i + '0');
 
 _putchar ('\n');
 }
