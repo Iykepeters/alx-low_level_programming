c9ba1b4475125993d8093e58962ed6b052fd61f9
Iykepeters <peterstickz@gmail.com>
2022-04-19 06:16:06 -0700

    task6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,20 +6,22 @@
  * Return: Always 0.
  */
 
-void print_line(int n)
-  
-{  
+void print_line(int n)  
+{
+
 int i;
 
 if (n > 0)
 {
+
 for (i = 0; i < n; i++)	
+
 {
 
 putchar (95);
 	  
 }
- 
+
 putchar ('\n');
 
 }
