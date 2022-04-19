596b30d75f0da294d79567ee0b14b8cd39f31130
Iykepeters <peterstickz@gmail.com>
2022-04-19 04:59:43 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,8 @@ int first_number;
 int second_number;
 int result;
 
-i = 0;	
+i = 0;
+		
 result = 0;
 while (i < 10)
 {
@@ -23,7 +24,9 @@ if (result < 10)
 second_number = result;
 }
 else
+
 {
+
 first_number = result / 10;  
 second_number = result % 10;
 putchar (first_number + '0');
