19d5d49086ecad5b40bfe550101ec2cf3e0b617a
Iykepeters <peterstickz@gmail.com>
2022-04-19 04:56:48 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -26,16 +26,16 @@ else
 {
 first_number = result / 10;  
 second_number = result % 10;
-_putchar (first_number + '0');
+putchar (first_number + '0');
 
 }
 
-_putchar (second_number + '0');
+putchar (second_number + '0');
 
 result++;
 }
 i++;
 result = 0;
-_putchar ('\n');
+putchar ('\n');
 }
 }
