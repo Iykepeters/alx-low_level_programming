1ab48b180ef8d3c3d4b1241708b313d224e4404f
Iykepeters <peterstickz@gmail.com>
2022-04-20 14:44:36 -0700

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,7 @@
 /**
  *reset_to_98 - updates an inteer value to 98.
  *
- *@n: Is a pointer to the integer value.
- * 
+ * @n: Is a pointer to the integer value. 
  *Return: void indicates correctness.
  */
 
