034e8128767859f3df38d6908e067faf65818f3a
Iykepeters <peterstickz@gmail.com>
2022-05-05 15:06:57 -0700

    returns a pointer

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "main.h"
+#include <stdlib>
 
-/** 
+/**
  *_strlen - count array
  *@s: array of elements
  *Return: 1
@@ -9,18 +10,22 @@
 int _strlen(char *s)
 
 {
+
 unsigned int i;
 i = 0;
+
 while (s[i] != '\0') /*count character of string*/
 {
 
 i++;
 
 }
+
 return (i);
+
 }
 
-/** 
+/**
  *_strcpy - copy arrays
  *@src: array of elements
  *@dest: dest array
@@ -32,17 +37,14 @@ char *_strcpy(char *dest, char *src)
 {
 
 int i = 0;
+
 while (src[i] != '\0')
-
 {
-
 dest[i] = src[i];
 i++;
-
 }
 dest[i] = '\0';
 return (dest);
-
 }
 
 /**
@@ -50,22 +52,32 @@ return (dest);
  *@str: array of elements
  *Return: pointer
  */
+
 char *_strdup(char *str)
 
 {
 
 char *dst;
 unsigned int size;
+
 if (str == 0)
+
 {
 return (NULL);
 }
+
 size = _strlen(str) + 1;
 dst = (char *) malloc(size *sizeof(char));
+
 if (dst == 0)
+
 {
+
 return (NULL);
+
 }
+
 _strcpy(dst, str);
 return (dst);
+
 }
