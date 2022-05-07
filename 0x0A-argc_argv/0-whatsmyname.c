e473d1700e8a92ea25caecbc596b88296a5ce50a
Iykepeters <peterstickz@gmail.com>
2022-05-07 13:59:59 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,13 +5,13 @@
  * @argc: Counts Arguments
  * @argv: Argument Vectors
  *
- * Return: Always 0 
+ * Return: Always 0 (success) 
  */
 
 int main(int argc, char *argv[])
 {
 
-(void) argc;
+if (argc > 0)
 printf("%s\n", argv[0]);
 
 return (0);
