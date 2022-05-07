2fa29c45c801bb55e3fc375797ce495067ec1db3
Iykepeters <peterstickz@gmail.com>
2022-05-07 14:38:54 -0700

    Task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -55,29 +55,19 @@ while (count < argc) /*Goes through the whole array*/
 if(check_num(argv[count]))
 
 {
-
 str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
-
 }
 
 /*Condition if one of the number contains symbols that are not digits*/
-
 else
 
 {
-
 printf("Error\n");
 return (1);
-
 }
-
 count++;
-
 }
-
 printf("%d\n", sum); /*print sum*/
-
 return (0);
-
 }
