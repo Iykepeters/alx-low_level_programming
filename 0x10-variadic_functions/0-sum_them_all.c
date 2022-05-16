8d1c12796566181c71cd909e44ecb4eb4d69f2c4
Iykepeters <peterstickz@gmail.com>
2022-05-16 14:13:17 -0700

    returns the sum of given parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,6 @@
  * sum_them_all - returns the sum of the inputed parameters
  * @n: amount of all present arguments
  * Return: sum of all parameters
- *
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -17,6 +16,7 @@ if (n == 0)
 return (0);
 
 va_start(valist, n);
+
 for (i = 0; i < n; i++)
 
 may += va_arg(valist, int);
