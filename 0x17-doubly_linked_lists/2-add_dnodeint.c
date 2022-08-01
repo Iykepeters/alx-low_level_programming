428947ed012003491fb51534c0592b14dc364e43
Iykepeters <peterstickz@gmail.com>
2022-08-01 07:54:18 -0700

    task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,48 +9,30 @@
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-
 {
 
 dlistint_t *new;
-
 if (head == NULL)
-
 return (NULL);
-
-
 
 /* malloc and set new node data */
-
 new = malloc(sizeof(struct dlistint_s));
-
 if (!new)
 return (NULL);
-
 new->n = n;
 
-
-
 /* account for empty linked list */
-
 if (*head == NULL)
-
 {
-
 *head = new;
-
 new->next = NULL;
-
 new->prev = NULL;
 return (new);
 }
 
-
 /* account for regular linked list */
-
 new->next = *head;
 new->prev = NULL;
-
 (*head)->prev = new;
 *head = new;
 
